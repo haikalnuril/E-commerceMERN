8496e00acf60455858ca167f4c04f915d4d6caa9
haikalnuril <haikal.nuril23@gmail.com>
2024-09-09 16:15:22 +0700

    feat: conncetion to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -10,3 +14,10 @@ app.use('/api/v1/auth', authRouter);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// connect DB mongoose
+mongoose.connect(process.env.DATABASE, {
+    
+}).then(() => {
+    console.log('Database connected');
+})
