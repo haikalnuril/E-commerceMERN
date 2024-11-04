ea4d6c53580e137ec11ad2ecdb3d68e03aad8f33
haikalnuril <haikal.nuril23@gmail.com>
2024-11-04 13:43:01 +0700

    feat: vercel

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(helmet());
+app.use(ExpressMongoSanitize());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
