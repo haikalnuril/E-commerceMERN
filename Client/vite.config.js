65906479c08d015ec8aa4b70d209594e0983ce23
haikalnuril <haikal.nuril23@gmail.com>
2025-01-03 07:24:34 +0700

    ready to deploy

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:3000",
+                target: "https://e-commerce-mern-be.vercel.app/",
                 changeOrigin: true,
             },
         },
